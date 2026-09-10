e6903dad8dbeff4304535b68f53d21c14e2d86fc
onnivaahtera <onni.vaahtera@gmail.com>
2026-09-10 17:42:43 +0300

    Revert "TS 1-3"
    This reverts commit 48d8f66e5823c96ecae1c16a290422f7c0e6056c.

--- TS-3/src/variables.ts
+++
@@ -1,9 +0,0 @@
-const apiUrl = 'https://sodexo-webscrape-r73sdlmfxa-lz.a.run.app/api/v1';
-
-const positionOptions = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-
-export {apiUrl, positionOptions};
